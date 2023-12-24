5da6d504cf766b2416a7100791f77dc95c923ff8
dimitri <dimitri.tsardakas@gmail.com>
2023-12-24 22:57:17 +1100

    add configuration files for the app

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import { type SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Papyrus",
+  description: "An AI powered PDF chatbot build on Next.js 13.",
+  url: "",
+  ogImage: "",
+  links: {
+    github: "",
+  },
+};
