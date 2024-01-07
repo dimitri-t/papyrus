38597b506638966b4ef46474868b655a39605d96
dimitri <dimitri.tsardakas@gmail.com>
2024-01-07 18:53:05 +1100

    comment out code that causes bug

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -4,7 +4,7 @@ import "simplebar-react/dist/simplebar.min.css";
 import { getCurrentUser } from "@/lib/session";
 import { SiteFooter } from "@/components/site-footer";
 import Navbar from "@/components/navbar";
-import { getUserSubscriptionPlan } from "@/lib/subscription";
+// import { getUserSubscriptionPlan } from "@/lib/subscription";
 
 export default async function RootLayout({
   children,
@@ -12,11 +12,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const user = await getCurrentUser();
-  const subscription = await getUserSubscriptionPlan();
+  // const subscription = await getUserSubscriptionPlan();
 
   return (
     <div className="flex min-h-screen flex-col">
-      <Navbar user={user} isSubscribed={subscription.isSubscribed} />
+      <Navbar user={user} isSubscribed={false} />
 
       <main className="flex-1">{children}</main>
       <SiteFooter />
