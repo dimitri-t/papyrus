7c501627c0800936dceb9a99da961a4b5c93f011
dimitri <dimitri.tsardakas@gmail.com>
2024-01-07 17:39:49 +1100

    remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,6 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
-import { getCurrentUser } from "@/lib/session";
-import { SiteFooter } from "@/components/site-footer";
-import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,8 +21,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const user = await getCurrentUser();
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
