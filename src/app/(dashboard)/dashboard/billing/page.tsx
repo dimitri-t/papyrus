31151331b26654d7f7255c75e71b930f33b8eeea
dimitri <dimitri.tsardakas@gmail.com>
2024-01-10 13:19:35 +1100

    update billing form

--- src/app/(dashboard)/dashboard/billing/page.tsx
+++ src/app/(dashboard)/dashboard/billing/page.tsx
@@ -1,10 +1,41 @@
 import BillingForm from '@/components/billing-form';
+import { DashboardHeader } from '@/components/dashboard-header';
+import { DashboardShell } from '@/components/dashboard-shell';
+import { Icons } from '@/components/icons';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { getUserSubscriptionPlan } from '@/lib/subscription';
 
-const Page = async () => {
+const BillingPage = async () => {
   const subscriptionPlan = await getUserSubscriptionPlan();
 
-  return <BillingForm subscriptionPlan={subscriptionPlan} />;
+  return (
+    <DashboardShell>
+      <DashboardHeader
+        heading="Billing"
+        text="Manage billing and your subscription plan."
+      />
+      <div className="grid max-w-3xl gap-8">
+        <Alert className="!pl-14">
+          <Icons.warning />
+          <AlertTitle>This is a demo app.</AlertTitle>
+          <AlertDescription>
+            Papyrus app is a demo app using a Stripe test environment. You can
+            find a list of test card numbers on the{' '}
+            <a
+              href="https://stripe.com/docs/testing#cards"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-8"
+            >
+              Stripe docs
+            </a>
+            .
+          </AlertDescription>
+        </Alert>
+        <BillingForm subscriptionPlan={subscriptionPlan} />
+      </div>
+    </DashboardShell>
+  );
 };
 
-export default Page;
+export default BillingPage;
--- src/components/billing-form.tsx
+++ src/components/billing-form.tsx
@@ -36,49 +36,42 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
     });
 
   return (
-    <div className="mx-auto w-full max-w-5xl px-2.5 md:px-20">
-      <form
-        className="mt-12"
-        onSubmit={(e) => {
-          e.preventDefault();
-          createStripeSession();
-        }}
-      >
-        <Card>
-          <CardHeader>
-            <CardTitle>Subscription Plan</CardTitle>
-            <CardDescription>
-              You are currently on the{' '}
-              <strong>
-                {subscriptionPlan.isSubscribed ? subscriptionPlan.name : 'free'}
-              </strong>{' '}
-              plan.
-            </CardDescription>
-          </CardHeader>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        createStripeSession();
+      }}
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>Subscription Plan</CardTitle>
+          <CardDescription>
+            You are currently on the <strong>{subscriptionPlan.name}</strong>{' '}
+            plan.
+          </CardDescription>
+        </CardHeader>
 
-          <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
-            <Button type="submit">
-              {isLoading ? (
-                <Loader2 className="mr-4 h-4 w-4 animate-spin" />
-              ) : null}
-              {subscriptionPlan.isSubscribed
-                ? 'Manage Subscription'
-                : 'Upgrade to PRO'}
-            </Button>
-
-            {subscriptionPlan.isSubscribed ? (
-              <p className="rounded-full text-xs font-medium">
-                {subscriptionPlan.isCanceled
-                  ? 'Your plan will be canceled on '
-                  : 'Your plan renews on '}
-                {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.MM.yyyy')}
-                .
-              </p>
+        <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
+          <Button type="submit">
+            {isLoading ? (
+              <Loader2 className="mr-4 h-4 w-4 animate-spin" />
             ) : null}
-          </CardFooter>
-        </Card>
-      </form>
-    </div>
+            {subscriptionPlan.isSubscribed
+              ? 'Manage Subscription'
+              : 'Upgrade to PRO'}
+          </Button>
+
+          {subscriptionPlan.isSubscribed ? (
+            <p className="rounded-full text-xs font-medium">
+              {subscriptionPlan.isCanceled
+                ? 'Your plan will be canceled on '
+                : 'Your plan renews on '}
+              {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.MM.yyyy')}.
+            </p>
+          ) : null}
+        </CardFooter>
+      </Card>
+    </form>
   );
 };
 
