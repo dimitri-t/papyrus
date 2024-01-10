031b9e3f6aafea1a9e269cb644a864eaa64e2738
dimitri <dimitri.tsardakas@gmail.com>
2024-01-10 12:25:41 +1100

    add back button and file title

--- src/app/(dashboard)/dashboard/[fileid]/page.tsx
+++ src/app/(dashboard)/dashboard/[fileid]/page.tsx
@@ -1,9 +1,13 @@
 import ChatWrapper from '@/components/chat/chat-wrapper';
+import { Icons } from '@/components/icons';
 import PdfRenderer from '@/components/pdf-renderer';
+import { buttonVariants } from '@/components/ui/button';
 import { authOptions } from '@/lib/auth';
 import { db } from '@/lib/db';
 import { getCurrentUser } from '@/lib/session';
 import { getUserSubscriptionPlan } from '@/lib/subscription';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 import { notFound, redirect } from 'next/navigation';
 
 interface PageProps {
@@ -32,7 +36,19 @@ const File = async ({ params }: PageProps) => {
 
   return (
     <div className="flex h-[calc(100vh-3.5rem)] flex-1 flex-col justify-between">
-      <div className="max-w-8xl mx-auto w-full grow lg:flex xl:px-2">
+      <div className="flex items-center space-x-10">
+        <Link
+          href="/dashboard"
+          className={cn(buttonVariants({ variant: 'ghost' }))}
+        >
+          <>
+            <Icons.chevronLeft className="mr-2 h-4 w-4" />
+            Back
+          </>
+        </Link>
+        <p className="text-muted-foreground text-sm">{file.name}</p>
+      </div>
+      <div className="max-w-8xl mx-auto w-full grow py-4 lg:flex xl:px-2">
         <div className="flex-1 xl:flex">
           <div className="px-2 py-3 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
             <PdfRenderer url={file.url} />
